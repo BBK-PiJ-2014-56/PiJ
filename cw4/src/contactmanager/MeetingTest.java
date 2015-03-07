f0f7980964751de276481d997277b3c6efdfb908
BBK-PiJ-2014-56 <BBK-PiJ-2014-56>
2015-03-07 12:18:18 +0000

    Initial Commit - started test classes

--- cw4/src/contactmanager/MeetingTest.java
+++ cw4/src/contactmanager/MeetingTest.java
@@ -1,21 +1,29 @@
 package contactmanager;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class MeetingTest {
 
+    @Before
+    public void setUp() throws Exception {
+        Meeting m = new Meeting(James, 1, 01/06/15);
+        Contact James = new Contact();
+        ContactManager cm = new ContactManager();
+        cm.addFutureMeeting(James, 01/06/15);
+    }
+
     @org.junit.Test
     public void testGetId() throws Exception {
-
+        assertEquals(1, m.getID());
     }
 
     @Test
     public void testGetDate() throws Exception {
-
+        assertEquals(01 / 06 / 15, m.getDate());
     }
 
     @Test
     public void testGetContacts() throws Exception {
-
-    }
+        assertEquals("James", m.GetContacts()}
 }
--- cw4/src/contactmanager/PastMeetingTest.java
+++ cw4/src/contactmanager/PastMeetingTest.java
@@ -6,11 +6,13 @@ public class PastMeetingTest {
 
     @Before
     public void setUp() throws Exception {
-
+        Meeting m = new Meeting(James, 1, 25/02/15);
+        ContactManager cm = new ContactManager();
+        cm.addMeetingNotes(1, "Birthday");
     }
 
     @org.junit.Test
     public void testGetNotes() throws Exception {
-
+        assertEquals("Birthday", m.getNotes());
     }
 }
