4afc43a42a05e1761642185e88d9260e61b967ff
BBK-PiJ-2014-56 <jthorn05@dcs.bbk.ac.uk>
2015-03-29 12:19:17 +0100

    Trying to get IO to create contacts.txt

--- cw4/src/contactmanager/DataIO.java
+++ cw4/src/contactmanager/DataIO.java
@@ -12,6 +12,7 @@ import java.nio.file.Paths;
 import java.util.Calendar;
 import java.util.List;
 import java.util.Scanner;
+import java.util.Set;
 
 /**
  * Created by James Thornton
@@ -21,17 +22,21 @@ public class DataIO {
     private final File contactsFile = new File("contacts.txt");
     final static Charset ENCODING = StandardCharsets.UTF_8;
 
-    public DataIO() {
+    public DataIO(Path fFilePath) throws IOException {
+        this.fFilePath = fFilePath;
         if (contactsFile.isFile()) {
             readFile();
+        } else {
+            contactsFile.getParentFile().mkdirs();
+            contactsFile.createNewFile();
         }
         Path path = Paths.get("contacts.txt");
-//        ReadWithScanner parser = new ReadWithScanner("C:\\Temp\\test.txt");
-//        parser.processLineByLine();
-//        log("Done.");
+        DataIO parser = new DataIO("contacts.txt");
+        parser.processLineByLine();
+        log("Done.");
     }
 
-    private void readFile() {
+    public void readFile() {
         try (Scanner fileReader = new Scanner(contactsFile)) {
             while (fileReader.hasNextLine()) {
                 //Readline
@@ -47,19 +52,19 @@ public class DataIO {
      Constructor.
      @param aFileName full name of an existing, readable file.
      */
-//    public ReadWithScanner(String aFileName){
-//        fFilePath = Paths.get(aFileName);
-//    }
-//
-//
-//    /** Template method that calls {@link #processLine(String)}.  */
-//    public final void processLineByLine() throws IOException {
-//        try (Scanner scanner =  new Scanner(fFilePath, ENCODING.name())){
-//            while (scanner.hasNextLine()){
-//                processLine(scanner.nextLine());
-//            }
-//        }
-//    }
+    public DataIO(String aFileName){
+        fFilePath = Paths.get(aFileName);
+    }
+
+
+    /** Template method that calls {@link #processLine(String)}.  */
+    public final void processLineByLine() throws IOException {
+        try (Scanner scanner =  new Scanner(fFilePath, ENCODING.name())){
+            while (scanner.hasNextLine()){
+                processLine(scanner.nextLine());
+            }
+        }
+    }
 
     /**
      Overridable method for processing lines in different ways.
@@ -86,7 +91,7 @@ public class DataIO {
         }
     }
 
-    public void writeFile(List<Contact> contactData, List<Meeting> meetingData) {
+    public void writeFile(Set<Contact> contactData, List<Meeting> meetingData) {
         Path path = Paths.get("contacts.txt");
         try (Scanner scanner = new Scanner(path, String.valueOf(ENCODING))){
             try (BufferedWriter writer = Files.newBufferedWriter(path, ENCODING)){
@@ -109,7 +114,7 @@ public class DataIO {
     }
 
     // PRIVATE
-    //private final Path fFilePath;
+    private final Path fFilePath;
     //private final static Charset ENCODING = StandardCharsets.UTF_8;
 
     private static void log(Object aObject){
